2b611bb623243ca3f956abfc48f53ef41f73a437
Akilanpalani <akilanpalani2001@gmail.com>
2024-03-08 19:48:01 +0530

    feat: :sparkles: Created ChannelId page, serverId page and added menu toogle for  sidebar

--- src/app/(main)/(routes)/servers/[serverId]/page.tsx
+++ src/app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -1,8 +1,48 @@
-const ServerIdPage = () => {
-  return(
-    <div>
-      ServerIdPage
-    </div>
-  )
+import { redirectToSignIn } from "@clerk/nextjs";
+
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { redirect } from "next/navigation";
+
+interface ServerIdPageProps {
+  params:{
+    serverId: string;
+  }
+}
+
+const ServerIdPage = async ({params}:ServerIdPageProps) => {
+  const profile = await currentProfile();
+
+  if(!profile){
+    return redirectToSignIn();
+  }
+
+  const server = await db.server.findUnique({
+    where:{
+      id:params.serverId,
+      members:{
+        some:{
+          profileId:profile.id
+        }
+      }
+    },
+    include:{
+      channels:{
+        where:{
+          name: "general"
+        },
+        orderBy:{
+          createdAt:"asc"
+        }
+      }
+    }
+  })
+
+  const intialChannel = server?.channels[0];
+
+  if(intialChannel?.name !== "general"){
+    return null;
+  }
+  return redirect(`/servers/${params.serverId}/channels/${intialChannel.id}`)
 }
 export default ServerIdPage;
