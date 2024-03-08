43997ef1437545217d7f8d1cd2d9019686de6bb0
Akilanpalani <akilanpalani2001@gmail.com>
2024-03-09 01:02:08 +0530

    feat: :sparkles: Added a socketIo and create api connection , used here useContext and provider

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,18 @@
+import { Server as NetServer, Socket } from 'net';
+import { NextApiResponse } from 'next';
+import { Server as SocketIOServer } from 'socket.io';
+
 import { Server, Profile, Member } from '@prisma/client';
+import { NextResponse } from 'next/server';
 
 export type ServerWithMembersWithProfiles = Server & {
   members: (Member & { profile: Profile })[];
+};
+
+export type NextApiResponseServerIo = NextApiResponse & {
+  socket: Socket & {
+    server: NetServer & {
+      io: SocketIOServer;
+    };
+  };
 };
