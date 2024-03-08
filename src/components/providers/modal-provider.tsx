85199b4064fa15d847e5cbba1cf673179a342ca9
Akilanpalani <akilanpalani2001@gmail.com>
2024-03-08 17:40:12 +0530

    feat: :sparkles: New Features, edit a channel, delete a channel, and setup the modal for those things

--- src/components/providers/modal-provider.tsx
+++ src/components/providers/modal-provider.tsx
@@ -2,8 +2,10 @@
 import { useEffect, useState } from 'react';
 
 import { CreateChannelModal } from '@/components/modals/create-channel-modal';
+import { DeleteChannelModal } from '@/components/modals/delete-channel-modal';
 import { CreateServerModal } from '@/components/modals/create-server-modal';
 import { DeleteServerModal } from '@/components/modals/delete-server-modal';
+import { EditChannelModal } from '@/components/modals/edit-channel-modal';
 import { LeaveServerModal } from '@/components/modals/leave-server-modal';
 import { EditServerModal } from '@/components/modals/edit-server-modal';
 import { MembersModal } from '@/components/modals/members-modal';
@@ -28,6 +30,8 @@ export const ModalProvider = () => {
       <CreateChannelModal />
       <LeaveServerModal />
       <DeleteServerModal />
+      <DeleteChannelModal />
+      <EditChannelModal />
     </>
   );
 };
