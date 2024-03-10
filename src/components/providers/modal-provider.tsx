0be058ee029f3aa451aa21f10e1eef0e2bb6dadf
Akilanpalani <akilanpalani2001@gmail.com>
2024-03-10 15:45:34 +0530

    feat: :sparkles: Created socket hook chat socket and created the delete message modal and prooviders

--- src/components/providers/modal-provider.tsx
+++ src/components/providers/modal-provider.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react';
 
 import { CreateChannelModal } from '@/components/modals/create-channel-modal';
 import { DeleteChannelModal } from '@/components/modals/delete-channel-modal';
+import { DeleteMessageModal } from '@/components/modals/delete-message-modal';
 import { CreateServerModal } from '@/components/modals/create-server-modal';
 import { DeleteServerModal } from '@/components/modals/delete-server-modal';
 import { EditChannelModal } from '@/components/modals/edit-channel-modal';
@@ -34,6 +35,7 @@ export const ModalProvider = () => {
       <DeleteChannelModal />
       <EditChannelModal />
       <MessageFileModal />
+      <DeleteMessageModal />
     </>
   );
 };
